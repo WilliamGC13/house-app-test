bfaf1f6dc725f9ff6bc0f860969bf6a0285483d5
WilliamGC13 <coffeywg@outlook.com>
2022-09-11 14:50:18 -0400

    example final commit

--- src/components/new-room-form.js
+++ src/components/new-room-form.js
@@ -13,21 +13,20 @@ export default class NewRoomForm extends React.Component {
         this.handleClick = this.handleClick.bind(this)
     }
 
-    //keeping track of the "name" variable as we type it out, passing it into state
     handleNameChange(e) {
         this.setState({nameValue: e.target.value})
     }
 
-    //same as above function, only for the "area" variable
+
     handleAreaChange(e) {
         this.setState({areaValue: e.target.value})
     }
 
-    //function called when the "Add Room" button is clicked. See the "addNewRoom" function in App.js
+
     handleClick(e) {
         this.props.addNewRoom(e, this.props.data,
             {name: this.state.nameValue, area: this.state.areaValue})
-        //reset the textboxes so they are empty once more
+
         this.setState({nameValue: "", areaValue: ""})
     }
 
